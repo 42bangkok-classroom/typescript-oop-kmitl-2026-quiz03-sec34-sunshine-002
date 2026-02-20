9fb1ed4839b71c08c6d59eaac2f97ce836dc5d4b
Sunshine <sunshine.17923@gmail.com>
2026-02-20 17:42:29 +0700

    1

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,31 @@
-export class User {}
+export class User {
+    static BIRTH_YEAR: number = 2000
+    private age: number = 0
+    public Firstname: string = ""
+    public Lastname: string = ""
+    set setage(age: number) {
+        if(age < 0) {
+            this.age = 0
+        } else {
+            this.age = age
+        }
+    }
+    get setage(): number {
+        return this.age
+    }
+    setFirstname(Fname: string) {
+        this.Firstname = Fname
+    }
+    setLastname(Lname: string) {
+        this.Lastname = Lname
+    }
+    getAge() {
+        return this.age
+    }
+    getFullName() {
+        return `${this.Firstname} ${this.Lastname}`
+    }
+    setAge(age: number) {
+        this.age = age
+    }
+}
